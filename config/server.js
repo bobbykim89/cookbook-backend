9e15a4d585ec3e7aafb03316267114ac597e3205
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-10-01 22:40:57 -0700

    initial configuration, graphql, cloudinary, content-type

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed04730c0ae8950c7973db01e4ea0a47'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '66877372-8964-4187-8b1d-107d9017d705'
+};
